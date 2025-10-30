2eb494e2e76b9240cf0e8d96eb394469738224c1
pashq96 <pashq-90@mail.ru>
2025-10-30 14:20:24 +0100

    Введение в HTTPX. Практическое задание: работа с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -8,21 +8,22 @@ login_payload = {
 
 # Выполняем запрос на аутентификацию
 login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
-login_response_data = login_response.json()
+token_response_data = login_response.json()
 
-# Выводим полученные токены
-print("Login response:", login_response_data)
-print("Status Code:", login_response.status_code)
+print(f'Код ответа {login_response.status_code}')
+print(f'Ответ содержит {token_response_data}')
 
-# Формируем payload для обновления токена
-refresh_payload = {
-    "refreshToken": login_response_data["token"]["refreshToken"]
+
+# Формируем header для вставки токена
+access_token = {
+    "Authorization": f'bearer {token_response_data["token"]["accessToken"]}'
 }
 
-# Выполняем запрос на обновление токена
-refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json=refresh_payload)
-refresh_response_data = refresh_response.json()
 
-# Выводим обновленные токены
-print("Refresh response:", refresh_response_data)
-print("Status Code:", refresh_response.status_code)
+me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=access_token)
+me_response_data = me_response.json()
+
+# Выводим мои данные код ответа
+
+print("Код ответа", me_response.status_code)
+print("JSON-ответ от сервера с данными о пользователе ", me_response_data)
