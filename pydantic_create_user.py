e5d8ec2dcf2d31d1536f0484f8f8d62d8417af7c
pashq96 <pashq-90@mail.ru>
2025-11-01 19:29:25 +0100

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,42 @@
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field, EmailStr, field_validator
+
+from tools.fakers import get_random_email
+
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+    # Проверка на то что str поле id подходит под структуру UUID
+    @field_validator('id')
+    def validate_uuid(cls, v):
+        try:
+            UUID(v)
+        except ValueError:
+            raise ValueError(f"{v} is not a valid UUID string")
+        return v
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание юзера.
+    """
+    email: EmailStr = Field(default_factory=get_random_email)
+    password: str = Field(default="password")
+    last_name: str = Field(alias="lastName", default="default_last_name")
+    first_name: str = Field(alias="firstName", default="default_first_name")
+    middle_name: str = Field(alias="middleName", default="default_middle_name")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
